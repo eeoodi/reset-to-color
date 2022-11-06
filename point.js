da3f042b0833bc0d798c69094089595c9a9915b5
eeoodi <74656146+eeoodi@users.noreply.github.com>
2022-11-06 15:25:14 +0900

    Create point.js

---
+++ point.js
@@ -0,0 +1,11 @@
+var points = 0
+
+function setpointtext() {
+  document.getElementById('point').textContent = points
+}
+
+while (true){
+  setTimeout(function() {
+    setpointtext()
+  }, 1000);
+}
